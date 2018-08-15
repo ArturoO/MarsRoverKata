a0a4beef7dc26125e7161beea1e3e0c8d132ba27
Artur Hoffman <thearturoo87@gmail.com>
2018-08-15 11:39:57 +0200

    Handle rotating of a rover.

--- MarsRoverKata/Rover.cs
+++ MarsRoverKata/Rover.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace MarsRoverKata
 {
-    class Rover
+    public class Rover
     {
+        protected char direction = 'N';
+        
+        public string GetCoordinates()
+        {
+            return "0:0:" + direction;
+        }
+
+        public void Execute(string commands)
+        {
+            for(var i=0; i<commands.Length; i++)
+            {
+                Rotate(commands[i]);
+            }
+        }
+
+        protected void Rotate(char command)
+        {
+            if (direction == 'N')
+            {
+                if (command == 'R')
+                    direction = 'E';
+                else if (command == 'L')
+                    direction = 'W';
+            }
+            else if (direction == 'E')
+            {
+                if (command == 'R')
+                    direction = 'S';
+                else if (command == 'L')
+                    direction = 'N';
+            }
+            else if (direction == 'S')
+            {
+                if (command == 'R')
+                    direction = 'W';
+                else if (command == 'L')
+                    direction = 'E';
+            }
+            else if (direction == 'W')
+            {
+                if (command == 'R')
+                    direction = 'N';
+                else if (command == 'L')
+                    direction = 'S';
+            }
+        }
     }
 }
--- MarsRoverKataTests/RoverTests.cs
+++ MarsRoverKataTests/RoverTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using MarsRoverKata;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,37 @@ namespace MarsRoverKataTests
 {
     public class RoverTests
     {
+        [Test]
+        public void GetCoordinates_RoverCreated_ReturnsInitialCoordinates()
+        {
+            string expected = "0:0:N";
+            Rover rover = new Rover();
+            Assert.That(rover.GetCoordinates(), Is.EqualTo(expected));
+        }
+
+        [TestCase("R", "0:0:E")]
+        [TestCase("RR", "0:0:S")]
+        [TestCase("RRR", "0:0:W")]
+        [TestCase("RRRR", "0:0:N")]
+        [TestCase("RRRRR", "0:0:E")]
+        public void GetCoordinates_RoverTurnsRight_ReturnsCorrectCoordinates(string commands, string expected)
+        {
+            Rover rover = new Rover();
+            rover.Execute(commands);
+            Assert.That(rover.GetCoordinates(), Is.EqualTo(expected));
+        }
+
+        [TestCase("L", "0:0:W")]
+        [TestCase("LL", "0:0:S")]
+        [TestCase("LLL", "0:0:E")]
+        [TestCase("LLLL", "0:0:N")]
+        [TestCase("LLLLL", "0:0:W")]
+        public void GetCoordinates_RoverTurnsLeft_ReturnsCorrectCoordinates(string commands, string expected)
+        {
+            Rover rover = new Rover();
+            rover.Execute(commands);
+            Assert.That(rover.GetCoordinates(), Is.EqualTo(expected));
+        }
 
     }
 }
