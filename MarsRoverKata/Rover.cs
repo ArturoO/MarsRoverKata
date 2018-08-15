26e5589661d2c3d4368d85ecda124315ab9a15cb
Artur Hoffman <thearturoo87@gmail.com>
2018-08-15 12:28:58 +0200

    Extract rotating functionality to the Direction class.

--- MarsRoverKata/Rover.cs
+++ MarsRoverKata/Rover.cs
@@ -1,58 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MarsRoverKata
 {
-    public class Rover
+    public partial class Rover
     {
-        protected char direction = 'N';
-        
+        Direction direction = new Direction();
+
         public string GetCoordinates()
         {
-            return "0:0:" + direction;
+            return "0:0:" + direction.Current;
         }
 
         public void Execute(string commands)
         {
             for(var i=0; i<commands.Length; i++)
             {
-                Rotate(commands[i]);
+                direction.Rotate(commands[i]);
             }
         }
 
-        protected void Rotate(char command)
-        {
-            if (direction == 'N')
-            {
-                if (command == 'R')
-                    direction = 'E';
-                else if (command == 'L')
-                    direction = 'W';
-            }
-            else if (direction == 'E')
-            {
-                if (command == 'R')
-                    direction = 'S';
-                else if (command == 'L')
-                    direction = 'N';
-            }
-            else if (direction == 'S')
-            {
-                if (command == 'R')
-                    direction = 'W';
-                else if (command == 'L')
-                    direction = 'E';
-            }
-            else if (direction == 'W')
-            {
-                if (command == 'R')
-                    direction = 'N';
-                else if (command == 'L')
-                    direction = 'S';
-            }
-        }
+        
     }
 }
